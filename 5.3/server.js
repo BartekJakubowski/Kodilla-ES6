43af63bd063b251821f41088c906ad33be713739
Kamil Pytel <k.s.pytel@gmail.com>
2017-09-01 12:31:53 +0100

    5.3

---
+++ 5.3/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+var stringifyFile;
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        fileContent = data;
+        res.send(data);
+    });
+});
+
+app.post('/updateNote/:note', function(res, req) {
+    stringifyFile = req.params.note;
+    fs.writeFile('./test.json', stringifyFile, function(req, res, next) {
+        //If (err) throw err;
+        console.log('File updated');
+    });
+});
+
+app.listen(3000);
