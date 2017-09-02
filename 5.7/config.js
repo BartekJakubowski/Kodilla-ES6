8814f5515b53d54f72ab004c7e155fbb4efddf00
Kamil Pytel <k.s.pytel@gmail.com>
2017-09-02 19:46:40 +0100

    5.7

---
+++ 5.7/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '525097458511-hnovrq96db2hcnq34606aau6rpuhp50s.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'FvbjUJLosX2QvAO5y7Ga3Qek',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
