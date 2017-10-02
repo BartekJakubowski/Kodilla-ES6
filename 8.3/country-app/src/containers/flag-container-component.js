e6979f750e57ff74ec2ae8f5a886468704552f7e
Kamil Pytel <k.s.pytel@gmail.com>
2017-10-02 14:28:42 +0100

    added continents

--- 8.3/country-app/src/containers/flag-container-component.js
+++ 8.3/country-app/src/containers/flag-container-component.js
@@ -29,7 +29,7 @@ class CountryFlagContainer extends Component {
 				<div className='search text-center'>
 					<input type='text' onChange={this.search.bind(this)} />
 				</div>
-				<CountryFlagList countries={this.props.visibleCountries} />
+				<CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry} />
 			</div>
 		)
 	}
